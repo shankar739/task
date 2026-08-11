ae7e007786c813404b8ae74c363363f5ccec0e7c
sankarreddi <reddyshankar55050@gmail.com>
2026-08-11 22:48:51 +0530

    Create demo.py and sample.py in dev branch

---
+++ demo.py
@@ -0,0 +1,5 @@
+def main():
+    print("This is demo.py in dev branch.")
+
+if __name__ == "__main__":
+    main()
---
+++ sample.py
@@ -0,0 +1,5 @@
+def main():
+    print("This is sample.py in dev branch.")
+
+if __name__ == "__main__":
+    main()
